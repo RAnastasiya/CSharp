ef712cb304be6fd6aca444df550b74ea6a74e95d
RAnastasiya <rizhko.anastasiya@gmail.com>
2017-12-19 01:40:06 +0200

    update

--- Solution3/Cat.cs
+++ Solution3/Cat.cs
@@ -1,12 +1,51 @@
 using System;
+using System.Globalization;
 
 namespace Solution3
 {
 	class Cat: Body{			
 
-		public string Type { get; set;}    
-		public int Age { get; set;}    
+		public string Type { get; set;}  
+		public void ReceiveType(){
+			Console.WriteLine ("Enter type: e.g. Siamese");
+			string readReceive;
+			readReceive = Console.ReadLine ();
+			this.Type = readReceive;
+		}
+		public int Age { get; set;}  
+		public void ReceiveAge(){
+			Console.WriteLine ("Enter age ");
+			while(true){
+				try {
+					Age = Convert.ToInt32(Console.ReadLine());
+					break;
+				} catch (Exception) {
+					Console.WriteLine ("You can enter only an integer");
+				}
+			}
+		}
 		public float Mood { get; set;} 
+		public void ReceiveMood(){
+			Console.WriteLine ("Enter mood: e.g. 3.4");
+			while (true) {
+				float number;
+				string value;
+				NumberStyles style;
+				CultureInfo culture;
+
+				value = Console.ReadLine ();
+				style = NumberStyles.AllowDecimalPoint;
+				culture = CultureInfo.InvariantCulture;
+				if (float.TryParse (value, style, culture, out number)){
+					Mood = number;
+					Console.WriteLine ("Converted '{0}' to {1}.", value, number);
+					break;
+				}else{
+					Console.WriteLine ("You can enter only an float");
+					Console.WriteLine("Unable to convert '{0}'.", value);
+				}
+			}
+		}
 		public override void Print(){
 			Console.WriteLine ("id: " + Id + 
 			                   "  Date: " + Date + 
--- Solution3/Program.cs
+++ Solution3/Program.cs
@@ -13,37 +13,50 @@ namespace Solution3{
 	class MainClas{
 		public static void Main (string[] args){			
 			Shelter shelter = new Shelter ();
-			Body[] body = new Body[shelter.CountDog + shelter.CountCat];
-			for (int i = 0; i < body.Length; i++) {
-				string readLine = shelter.Choice ();
-				if (readLine.ToLower() == "dog"){
-					Dog dog = new Dog ();
-					Console.Clear ();
-					body[i] = dog;
-					dog.Id = i;
-					dog.ReceiveDate();
-					PrintArray (i, body);
-					dog.ReceiveSex();
-					PrintArray (i, body);
-					dog.ReceiveIdentified ();
-					PrintArray (i, body);
-					dog.ReceiveAge ();
-					PrintArray (i, body);
-					continue;
-				}else if(readLine == "cat"){
-					Cat cat = new Cat ();
-					body[i] = cat;
-					cat.Id = i;
-					PrintArray (i, body);
-					Console.WriteLine ("Caaaaaaaaaaat");
-					continue;
-				}else if(readLine == "not"){
-					Console.WriteLine ("Recording is complete");
-					break;
-				}else{
-					Console.WriteLine ("You entered an incorrect value");
-					i--;
-					continue;
+			int quanttity = shelter.CountDog + shelter.CountCat;
+			if (quanttity == 0) {
+				Console.WriteLine ("There is no place in the shelter((  " +
+				"Recording is complete");
+			} else {
+				Body[] body = new Body[quanttity];
+				for (int i = 0; i < body.Length; i++) {
+					string readLine = shelter.Choice ();
+					if (readLine.ToLower() == "dog"){
+						Dog dog = new Dog ();
+						Console.Clear ();
+						body[i] = dog;
+						dog.Id = i;
+						dog.ReceiveDate();
+						PrintArray (i, body);
+						dog.ReceiveSex();
+						PrintArray (i, body);
+						dog.ReceiveIdentified ();
+						PrintArray (i, body);
+						dog.ReceiveAge ();
+						PrintArray (i, body);
+						continue;
+					}else if(readLine == "cat"){
+						Cat cat = new Cat ();
+						body[i] = cat;
+						cat.Id = i;
+						PrintArray (i, body);
+						cat.ReceiveDate ();
+						PrintArray (i, body);
+						cat.ReceiveType ();
+						PrintArray (i, body);
+						cat.ReceiveAge ();
+						PrintArray (i, body);
+						cat.ReceiveMood ();
+						PrintArray (i, body);
+						continue;
+					}else if(readLine == "not"){
+						Console.WriteLine ("Recording is complete");
+						break;
+					}else{
+						Console.WriteLine ("You entered an incorrect value");
+						i--;
+						continue;
+					}
 				}
 			}
 		}
